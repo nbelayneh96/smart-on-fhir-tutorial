5030d7faa7a6920b9260f79e26aecb632f73e9dd
nbelayneh96 <108300074+nbelayneh96@users.noreply.github.com>
2022-06-28 12:47:09 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -25,6 +25,8 @@
         $.when(pt, obv).fail(onError);
 
         $.when(pt, obv).done(function(patient, obv) {
+          console.log(patient);
+          console.log(obv);
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
